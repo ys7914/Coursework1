22f559d4777c517a850e254a84c170ef76912680
xingzi9961 <zhuxingzi@gmail.com>
2017-02-06 22:28:38 +0000

    Update ConnectNetwork.py

--- ConnectNetwork.py
+++ ConnectNetwork.py
@@ -1,7 +1,7 @@
 import network
 ap_if=network.WLAN(network.AP_IF)
-ap_if.active(False)
-sta_if.active(True)
+ap_if.active(False)#turn off automatic receiving
+sta_if.active(True)#turn on transmitting
 sta_if=network.WLAN(network.STA_IF)
-sta_if.connect('EEERover','exhibition')
+sta_if.connect('EEERover','exhibition')#connect to internet
 sta_if.isconnected()
