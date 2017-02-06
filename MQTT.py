37be37e98567020f3e04c6b9fce75cf84781d99c
xingzi9961 <zhuxingzi@gmail.com>
2017-02-06 22:27:39 +0000

    Update MQTT.py

--- MQTT.py
+++ MQTT.py
@@ -1,5 +1,5 @@
 from umqtt.simple import MQTTClient
-client=MQTTClient("XYX17","192.168.0.10")//client ID, followed by broker addr
+client=MQTTClient("XYX17","192.168.0.10")#client ID, followed by broker addr
 client.connect()
 client.publish("esys/XYX17",bytes("message",'utf-8'))
 
